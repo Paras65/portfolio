7d8b8ed7369885c8a8e5f24052e1136b8278d0f0
paras <paraskumarsahu65@gmail.com>
2025-05-07 23:10:04 +0530

    vite cofig base setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Replace 'portfolio' with your repository name
   plugins: [react()],
-})
+});
