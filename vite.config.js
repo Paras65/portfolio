05bf9103e109ae2ca668782949adac03c0e373bf
paras <paraskumarsahu65@gmail.com>
2025-06-14 16:34:41 +0530

    mj

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio/', // Replace 'portfolio' with your repository name
+  base: '/portfolio/', // 
   plugins: [react()],
 });
