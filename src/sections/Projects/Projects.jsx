ad32f824e35a2d1ed39afebc52b0832539754f74
paras <paraskumarsahu65@gmail.com>
2025-07-12 10:42:21 +0530

    new project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import styles from './ProjectsStyles.module.css';
 import ProjectCard from '../../common/ProjectCard';
 import pj from '../../assets/Untitled.svg'
+import gk from '../../assets/Black & Blue Creative Digital Camera Photo Studio Logo.png';
 
 function Projects() {
   return (
@@ -22,7 +23,13 @@ using React.js"
           p="Created a portfolio website for Alen Sahu Art using React.js"
           className="responsiveImage"
         />
-       
+        <ProjectCard
+          src={gk}
+          link="https://gk-digital-studio.vercel.app//"
+          h3="GK Digital Studio"
+          p="Developed a portfolio website for GK Digital Studio using React.js"
+          className="responsiveImage"
+        />
       </div>
     </section>
   );
